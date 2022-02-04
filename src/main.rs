b2373decadb24afd5d124982d3c293d853539620
Saul Diez-Guerra <saul@diezguerra.com>
2022-02-03 19:38:48 -0800

    Double port echo server

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use async_std::{
 
 type Result<T> = Rs<T, Box<dyn std::error::Error + Send + Sync>>;
 
-async fn echo_loop(addr: impl ToSocketAddrs) -> Result<()> {
+async fn bind_server(addr: impl ToSocketAddrs) -> Result<()> {
 
     let listener = TcpListener::bind(addr).await?;
     let mut incoming = listener.incoming();
@@ -16,12 +16,13 @@ async fn echo_loop(addr: impl ToSocketAddrs) -> Result<()> {
         let stream = stream?;
         let peer = stream.peer_addr()?;
         println!("Accepting from: {}", peer);
-        let _handle = task::spawn(connection_loop(stream));
+        let _handle = task::spawn(echo_server(stream));
     }
+
     Ok(())
 }
 
-async fn connection_loop( mut stream: TcpStream) -> Result<()> {
+async fn echo_server(mut stream: TcpStream) -> Result<()> {
     loop {
         let mut reader = BufReader::new(&stream);
         let mut line = String::new();
@@ -32,9 +33,12 @@ async fn connection_loop( mut stream: TcpStream) -> Result<()> {
 
 fn main() -> Result<()> {
 
-    const BIND_ADDRESS: &str = "127.0.0.1:7878";
+    const SMTP_ADDRESS: &str = "127.0.0.1:7878";
+    const HTTP_ADDRESS: &str = "127.0.0.1:7879";
 
-    let fut = echo_loop(BIND_ADDRESS);
-    task::block_on(fut)
-
+    let smtp = task::spawn(bind_server(SMTP_ADDRESS));
+    let http = task::spawn(bind_server(HTTP_ADDRESS));
+    task::block_on(smtp)?;
+    task::block_on(http)?;
+    Ok(())
 }
