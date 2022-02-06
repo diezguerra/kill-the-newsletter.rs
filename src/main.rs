435b1f666057b62a88f2e663a866b808b9c4f8b3
Saul Diez-Guerra <saul@diezguerra.com>
2022-02-05 21:09:34 -0800

    echo server with tokio

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,34 @@
-use std::result::Result as Rs;
-use async_std::{
-    io::BufReader,
-    prelude::*,
-    task,
-    net::{TcpStream, TcpListener, ToSocketAddrs},
-};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
-type Result<T> = Rs<T, Box<dyn std::error::Error + Send + Sync>>;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    const ADDRESS: &str = "127.0.0.1:7878";
 
-async fn bind_server(addr: impl ToSocketAddrs) -> Result<()> {
+    let listener = TcpListener::bind(ADDRESS).await?;
 
-    let listener = TcpListener::bind(addr).await?;
-    let mut incoming = listener.incoming();
-    while let Some(stream) = incoming.next().await {
-        match stream {
-            Ok(stream) => {
-                let _handle = task::spawn(echo_server(stream));
-            },
-            Err(e) => {
-                println!("Connection error: {}", e);
-            }
-        }
-    }
-
-    Ok(())
-}
-
-async fn echo_server(mut stream: TcpStream) -> Result<()> {
-    let peer = stream.peer_addr()?;
-    println!("Accepting from: {}", peer);
     loop {
-        let mut reader = BufReader::new(&stream);
-        let mut line = String::new();
-        reader.read_line(&mut line).await?;
-        stream.write_all(format!("Received {}", line).as_bytes()).await?;
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 32];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
     }
-}
-
-fn main() -> Result<()> {
-
-    const SMTP_ADDRESS: &str = "127.0.0.1:7878";
-    const HTTP_ADDRESS: &str = "127.0.0.1:7879";
-
-    let smtp = task::spawn(bind_server(SMTP_ADDRESS));
-    let http = task::spawn(bind_server(HTTP_ADDRESS));
-    task::block_on(smtp)?;
-    task::block_on(http)?;
-    Ok(())
 }
