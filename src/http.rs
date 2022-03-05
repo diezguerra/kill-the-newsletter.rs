9add94a02aa9e32a87b640da8a5a3c86c54ca899
Saul Diez-Guerra <saul@diezguerra.com>
2022-03-05 13:06:17 -0800

    feat: atom content-type, proper error handling

--- src/http.rs
+++ src/http.rs
@@ -1,8 +1,9 @@
 use askama::Template;
 use axum::{
+    self, body,
     extract::{Extension, Path},
-    http::StatusCode,
-    response::IntoResponse,
+    http::{self, StatusCode},
+    response::{IntoResponse, Response},
     routing::{get, post},
     AddExtensionLayer, Json, Router,
 };
@@ -13,6 +14,16 @@ use crate::models::entry::{find_reference, Entry};
 use crate::models::feed::{get_title_by_reference, NewFeed};
 use crate::time::filters;
 use crate::vars::{EMAIL_DOMAIN, WEB_URL};
+
+pub fn build_app(
+    pool_arc: Arc<r2d2::Pool<SqliteConnectionManager>>,
+) -> axum::routing::IntoMakeService<Router> {
+    Router::new()
+        .route("/", post(create_feed))
+        .route("/:reference", get(get_reference))
+        .layer(AddExtensionLayer::new(pool_arc))
+        .into_make_service()
+}
 
 async fn create_feed(
     item: Json<NewFeed>,
@@ -37,7 +48,7 @@ struct AtomTemplate {
 async fn get_reference(
     Path(reference): Path<String>,
     Extension(pool_arc): Extension<Arc<r2d2::Pool<SqliteConnectionManager>>>,
-) -> (StatusCode, impl IntoResponse) {
+) -> Result<impl IntoResponse, KtnError> {
     // This block needs to be here cause matchit is too fast to be flexible
     let no_ext_ref: String = String::from({
         if reference.ends_with(".xml") {
@@ -51,11 +62,11 @@ async fn get_reference(
     let mut conn = pool.get().expect("Couldn't get database connection");
     let entries = match find_reference(&no_ext_ref, &mut conn) {
         Ok(entries) => entries,
-        Err(_) => return (StatusCode::NOT_FOUND, String::from("Not found")),
+        Err(_) => return Err(KtnError::InternalServerError),
     };
 
     if entries.len() == 0 {
-        return (StatusCode::NOT_FOUND, String::from("Not found"));
+        return Err(KtnError::NotFoundError);
     }
 
     let title = match get_title_by_reference(&reference, &mut conn) {
@@ -63,26 +74,54 @@ async fn get_reference(
         _ => String::from("No feed title found"),
     };
 
-    (
-        StatusCode::OK,
-        AtomTemplate {
-            web_url: Box::new(String::from(WEB_URL)),
-            email_domain: Box::new(String::from(EMAIL_DOMAIN)),
-            feed_title: Box::new(title),
-            feed_reference: Box::new(reference),
-            entries: entries,
-        }
-        .render()
-        .expect("Failed to render Atom"),
-    )
+    let feed = AtomTemplate {
+        web_url: Box::new(String::from(WEB_URL)),
+        email_domain: Box::new(String::from(EMAIL_DOMAIN)),
+        feed_title: Box::new(title),
+        feed_reference: Box::new(reference),
+        entries: entries,
+    }
+    .render()
+    .unwrap_or(String::from("Couldn't render Atom feed template"));
+
+    Ok(Response::builder()
+        .status(StatusCode::OK)
+        .header(
+            http::header::CONTENT_TYPE,
+            http::HeaderValue::from_static("application/atom+xml"),
+        )
+        .body(body::boxed(body::Full::from(feed)))
+        .unwrap())
 }
 
-pub fn build_app(
-    pool_arc: Arc<r2d2::Pool<SqliteConnectionManager>>,
-) -> axum::routing::IntoMakeService<Router> {
-    Router::new()
-        .route("/", post(create_feed))
-        .route("/:reference", get(get_reference))
-        .layer(AddExtensionLayer::new(pool_arc))
-        .into_make_service()
+#[derive(Debug)]
+pub enum KtnError {
+    NotFoundError,
+    InternalServerError,
+}
+
+impl std::fmt::Display for KtnError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for KtnError {}
+
+impl IntoResponse for KtnError {
+    fn into_response(self) -> Response {
+        let body = match self {
+            KtnError::NotFoundError => {
+                body::boxed(body::Full::from("Not Found"))
+            }
+            _ => body::boxed(body::Full::from("Undertermined error")),
+        };
+
+        let status = match self {
+            KtnError::NotFoundError => StatusCode::NOT_FOUND,
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+
+        Response::builder().status(status).body(body).unwrap()
+    }
 }
